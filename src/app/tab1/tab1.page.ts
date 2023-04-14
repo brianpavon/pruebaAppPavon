a75b0183a5f59d4c1710656f253ce0b5650776ac
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-04-13 21:12:53 -0300

    dejo el html del login

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent,CommonModule],
 })
 export class Tab1Page {
-  constructor() {}
+  nombre !: string ;
+  apellido !: string ;
+  public mostrar : boolean = false;
+  direccion !: string ;
+  
+
+  constructor() {
+    //this.mostrar=false;
+  }
+
+  MostrarDatos(nombre:any, apellido:any){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.mostrar = true;
+  }
 }
