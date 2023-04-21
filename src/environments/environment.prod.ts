5c20c0f380f3647dcbe9d946a0dc802a1b54d23e
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-04-21 18:03:10 -0300

    cambio config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyCSHtAaDNJbKaZ4uellwcf1ZSuZ9AYv-gw",
+    authDomain: "pruebaapppavon.firebaseapp.com",
+    projectId: "pruebaapppavon",
+    storageBucket: "pruebaapppavon.appspot.com",
+    messagingSenderId: "755219631011",
+    appId: "1:755219631011:web:82eb523601b622a272e816"
+  },
   production: true
 };
